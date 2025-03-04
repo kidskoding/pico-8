5a938a52f7475018f4cff88e3ab49f58015e4ff6
Anirudh Konidala <kidskodingclub@gmail.com>
2025-03-03 18:14:39 -0600

    demo code from the sdl3 docs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use std::time::Duration;
+use sdl3::event::Event;
+use sdl3::keyboard::Keycode;
+use sdl3::pixels::Color;
+
+const WIDTH: u32 = 128;
+const HEIGHT: u32 = 128;
+
 fn main() {
-    println!("Hello, world!");
+    let sdl_context = sdl3::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem.window("Pico-8", WIDTH, HEIGHT)
+        .position_centered()
+        .build()
+        .unwrap();
+    
+    let mut canvas = window.into_canvas();
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+    
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i = 0;
+    
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} | 
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+        
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
 }
